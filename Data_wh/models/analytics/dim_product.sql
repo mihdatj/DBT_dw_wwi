8e352907efd88675d190a3610fcf9f9b0b665120
mihdatj <lonnhanhadoi@gmail.com>
2025-11-21 09:38:07 +0700

    cast type column

--- Data_wh/models/analytics/dim_product.sql
+++ Data_wh/models/analytics/dim_product.sql
@@ -1,5 +1,5 @@
 SELECT
-  StockItemID AS Product_key
-  , StockItemName AS Product_name
-  , Brand AS Brand_name
+  Cast(StockItemID AS INTEGER) AS Product_key
+  , Cast(StockItemName AS STRING) AS Product_name
+  , Cast(Brand AS String) AS Brand_name
 FROM `data-wh-mihdatj1.Wide_World_Importers.Warehouse_StockItems`
--- Data_wh/models/analytics/fact_sales_order_line.sql
+++ Data_wh/models/analytics/fact_sales_order_line.sql
@@ -1,6 +1,7 @@
 SELECT
-  OrderLineID AS Sales_order_line_id
-  , Quantity
-  , UnitPrice AS Unit_Price
+  Cast(OrderLineID AS INTEGER) AS Sales_order_line_id
+  , Cast(StockItemID AS STRING) AS Product_key
+  , Cast(Quantity as INTEGER) AS Quantity 
+  , Cast(UnitPrice as NUMERIC) AS UnitPrice 
   , Quantity * UnitPrice AS Grossamount
 FROM `data-wh-mihdatj1.Wide_World_Importers.Sales_OrderLines`
