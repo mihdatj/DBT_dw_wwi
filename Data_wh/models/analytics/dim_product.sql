c51ac8394811fa102a795e155141d5e181aa918e
mihdatj <lonnhanhadoi@gmail.com>
2025-11-21 10:05:37 +0700

    cte rename, cast type dim_product

--- Data_wh/models/analytics/dim_product.sql
+++ Data_wh/models/analytics/dim_product.sql
@@ -1,5 +1,27 @@
+WITH dim_product__source AS (
+    SELECT
+        *
+    FROM `data-wh-mihdatj1.Wide_World_Importers.Warehouse_StockItems`
+)
+
+, dim_product__rename AS(
+    SELECT
+        StockItemID AS Product_key
+        , StockItemName AS Product_name
+        , Brand AS Brand_name
+    FROM dim_product__source
+)
+
+, dim_product__cast_type AS(
+    SELECT
+    Cast(Product_key AS INTEGER) AS Product_key
+    , Cast(Product_name AS STRING) AS Product_name
+    , Cast(Brand AS String) AS Brand_name
+    FROM dim_product__rename
+)
+
 SELECT
-  Cast(StockItemID AS INTEGER) AS Product_key
-  , Cast(StockItemName AS STRING) AS Product_name
-  , Cast(Brand AS String) AS Brand_name
-FROM `data-wh-mihdatj1.Wide_World_Importers.Warehouse_StockItems`
+   Product_key
+  , Product_name
+  , Brand_name
+FROM dim_product__cast_type
