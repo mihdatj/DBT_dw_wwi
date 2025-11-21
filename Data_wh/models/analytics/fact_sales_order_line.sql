2a8645ef8d1300593effed8315a10eee6b49427c
mihdatj <lonnhanhadoi@gmail.com>
2025-11-21 09:58:53 +0700

    cte rename, cast type fact_sales_order_line

--- Data_wh/models/analytics/fact_sales_order_line.sql
+++ Data_wh/models/analytics/fact_sales_order_line.sql
@@ -1,7 +1,32 @@
-SELECT
-  Cast(OrderLineID AS INTEGER) AS Sales_order_line_id
-  , Cast(StockItemID AS STRING) AS Product_key
+WITH fact_sales_order_line__source AS (
+  SELECT
+    *
+  FROM `data-wh-mihdatj1.Wide_World_Importers.Sales_OrderLines`
+
+)
+, fact_sales_order_line__rename AS (
+  SELECT
+    OrderLineID  AS Sales_order_line_id
+    , StockItemID  AS Product_key
+    , Quantity   
+    , UnitPrice  
+  FROM fact_sales_order_line__source
+)
+
+, fact_sales_order_line__cast_type AS (
+  SELECT
+  Cast(Sales_order_line_id AS INTEGER) AS Sales_order_line_id
+  , Cast(Product_key AS STRING) AS Product_key
   , Cast(Quantity as INTEGER) AS Quantity 
   , Cast(UnitPrice as NUMERIC) AS UnitPrice 
+  FROM fact_sales_order_line__rename
+)
+
+
+SELECT
+   Sales_order_line_id
+  , Product_key
+  , Quantity 
+  , UnitPrice 
   , Quantity * UnitPrice AS Grossamount
-FROM `data-wh-mihdatj1.Wide_World_Importers.Sales_OrderLines`
+FROM fact_sales_order_line__cast_type
