b37d3ffb61222c77f758cbfd67c9371004b2f281
mihdatj <lonnhanhadoi@gmail.com>
2025-11-27 15:20:59 +0700

    create stg_fact_sale_order and rename, cast type

---
+++ Data_wh/models/staging/stg_fact_sales_order.sql
@@ -0,0 +1,22 @@
+WITH stg_fact_sales_order__source AS (  
+    SELECT 
+        * 
+    FROM `data-wh-mihdatj1.Wide_World_Importers.Sales-Orders`
+)
+, stg_fact_sales_order__rename AS (
+    SELECT 
+        OrderID AS Sales_Order_key
+        , CustomerID AS Customer_key
+    FROM stg_fact_sales_order__source
+)
+
+, stg_fact_sales_order__cast_type AS (
+    SELECT
+        Cast( Sales_Order_key AS INTEGER) AS Sales_Order_key
+        , Cast(Customer_key AS INTEGER) AS Customer_key
+    FROM stg_fact_sales_order__rename
+) 
+SELECT
+    Sales_Order_key
+    , Customer_key
+FROM stg_fact_sales_order__cast_type
