2d745ccd1bfe7600f849903c60dca9c493f63160
mihdatj <lonnhanhadoi@gmail.com>
2025-11-27 15:06:15 +0700

    add orderid fact_sales_order_line

--- Data_wh/models/analytics/fact_sales_order_line.sql
+++ Data_wh/models/analytics/fact_sales_order_line.sql
@@ -6,7 +6,8 @@ WITH fact_sales_order_line__source AS (
 )
 , fact_sales_order_line__rename AS (
   SELECT
-    OrderLineID  AS Sales_order_line_id
+    OrderLineID  AS Sales_order_line_key
+    , OrderID AS Order_key
     , StockItemID  AS Product_key
     , Quantity   
     , UnitPrice  
@@ -15,7 +16,8 @@ WITH fact_sales_order_line__source AS (
 
 , fact_sales_order_line__cast_type AS (
   SELECT
-  Cast(Sales_order_line_id AS INTEGER) AS Sales_order_line_id
+  Cast(Sales_order_line_key AS INTEGER) AS Sales_order_line_key
+  , Cast(Order_key AS INTEGER) as Order_key
   , Cast(Product_key AS STRING) AS Product_key
   , Cast(Quantity as INTEGER) AS Quantity 
   , Cast(UnitPrice as NUMERIC) AS UnitPrice 
@@ -24,7 +26,8 @@ WITH fact_sales_order_line__source AS (
 
 
 SELECT
-   Sales_order_line_id
+  Sales_order_line_key
+  , Order_key
   , Product_key
   , Quantity 
   , UnitPrice 
