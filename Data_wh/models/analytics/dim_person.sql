94f108b5374f5bf9bac36a6409794d3f04b97b8b
mihdatj <lonnhanhadoi@gmail.com>
2025-11-29 04:38:41 +0700

    rename, cast_type dim_person

--- Data_wh/models/analytics/dim_person.sql
+++ Data_wh/models/analytics/dim_person.sql
@@ -1,3 +1,24 @@
+WITH dim_person__source AS (
+    SELECT 
+    *
+    FROM `data-wh-mihdatj1.Wide_World_Importers.Application_People`
+)
+
+, dim_person__rename AS (
+    SELECT
+        PersonID AS person_key
+        , FullName 
+    FROM dim_person__source
+)
+
+, dim_person__cast_type AS (
+    SELECT 
+        Cast(person_key AS INTEGER) AS person_key
+        , Cast(FullName AS STRING) AS FullName
+    FROM dim_person__rename
+)
+
 SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.application__people`
+    person_key
+    , FullName
+FROM dim_person__cast_type
