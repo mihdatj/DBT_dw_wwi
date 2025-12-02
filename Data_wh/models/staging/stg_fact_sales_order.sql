ef9a35f80fe40ee2879234aeac2dd88ca2bdda92
mihdatj <lonnhanhadoi@gmail.com>
2025-12-02 23:58:01 +0700

    add 3 column stg_fact_sales_order

--- Data_wh/models/staging/stg_fact_sales_order.sql
+++ Data_wh/models/staging/stg_fact_sales_order.sql
@@ -9,6 +9,9 @@ WITH stg_fact_sales_order__source AS (
         , CustomerID AS Customer_key
         , PickedByPersonID AS Picked_By_Person_key
         , OrderDate
+        , BackorderOrderID AS Back_Order_key
+        , ExpectedDeliveryDate AS Expected_Delivery_Date
+        , IsUndersupplyBackordered AS Is_Under_supply_Backordered_boolean
     FROM stg_fact_sales_order__source
 )
 
@@ -18,11 +21,29 @@ WITH stg_fact_sales_order__source AS (
         , Cast(Customer_key AS INTEGER) AS Customer_key
         , SAFE_Cast(Picked_By_Person_key AS INTEGER) AS Picked_By_Person_key
         , Cast(OrderDate AS DATE) AS OrderDate
+        , Cast(Back_Order_key AS INTEGER) AS Back_Order_key
+        , CAST(Expected_Delivery_Date AS DATE) AS Expected_Delivery_Date
+        , CAST(Is_Under_supply_Backordered_boolean AS BOOLEAN) AS Is_Under_supply_Backordered_boolean
     FROM stg_fact_sales_order__rename
 ) 
+
+, stg_fact_sales_order__conver_boolean AS (
+    SELECT
+        *
+        , CASE
+            WHEN Is_Under_supply_Backordered_boolean IS TRUE THEN 'Under Supply Backordered'
+            WHEN Is_Under_supply_Backordered_boolean IS FALSE THEN 'NOT Under Supply Backordered'
+            WHEN Is_Under_supply_Backordered_boolean IS NULL THEN 'Undefined'
+        ELSE 'Invalid' END AS Is_Under_supply_Backordered
+    FROM stg_fact_sales_order__cast_type
+)
+
 SELECT
     Sales_Order_key
     , Customer_key
-    , Coalesce(Picked_By_Person_key,0) AS Picked_By_Person_key
+    , Coalesce(Picked_By_Person_key, 0) AS Picked_By_Person_key
+    , COALESCE(Back_Order_key, 0) AS Back_Order_key
     , OrderDate
-FROM stg_fact_sales_order__cast_type
+    , Expected_Delivery_Date 
+    , Is_Under_supply_Backordered_boolean
+FROM stg_fact_sales_order__conver_boolean
