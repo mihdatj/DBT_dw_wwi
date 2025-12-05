e32406b1327ed0b437dfd08825946a6ea5ca0c06
mihdatj <lonnhanhadoi@gmail.com>
2025-12-06 03:30:02 +0700

    create stg_Supplier_Categories rename, cast type

---
+++ Data_wh/models/staging/stg_Supplier_Categories.sql
@@ -0,0 +1,23 @@
+WITH stg_Supplier_Categories__source AS (
+    SELECT
+        * 
+    FROM `data-wh-mihdatj1.Wide_World_Importers.Purchasing_SupplierCategorie`
+)
+
+, stg_Supplier_Categories__rename AS (
+    SELECT
+        supplier_category_id AS supplier_category_key
+        , supplier_category_name
+    FROM stg_Supplier_Categories__source
+)
+
+, stg_Supplier_Categories__cast_type AS (
+    SELECT
+        Cast(supplier_category_key AS INTEGER) AS supplier_category_key
+        , Cast(supplier_category_name AS String) AS supplier_category_name
+    FROM stg_Supplier_Categories__rename
+)
+SELECT
+    supplier_category_key
+    , supplier_category_name
+FROM stg_Supplier_Categories__cast_type
